213eefda9a8fc921cd8ca82c55a15a082ceb9790
schaarmi <Michael_Schaar@web.de>
2016-06-08 18:01:30 +0200

    Specfile für nfs-server

--- specfiles/spec/nfs_common/default.rb
+++ specfiles/spec/nfs_common/default.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe package('nfs-common') do
   it {should be_installed}
 end
+
+describe command('showmount -e 192.168.6.4') do
+    its(:stdout) { should match /\/var\/nfs\s+192\.168\.6\.3/ }
+end
--- specfiles/spec/nfs_kernel/default.rb
+++ specfiles/spec/nfs_kernel/default.rb
@@ -3,3 +3,25 @@ require 'spec_helper'
 describe package('nfs-kernel-server') do
   it {should be_installed}
 end
+
+
+describe service('nfs-kernel-server') do
+  it { should be_running }
+end
+
+describe file('/var/nfs') do
+  it { should be_directory }
+  it { should be_owned_by 'vagrant' }
+  it { should be_grouped_into 'vagrant' }
+  it { should be_executable.by_user('vagrant') }
+  it { should be_writable.by_user('vagrant') }
+  it { should be_readable.by_user('vagrant') }
+end
+
+describe file('/etc/exports') do
+  it { should be_file }
+  its(:content) { should match /^\/var\/nfs/ }
+end
+describe command('showmount -e localhost') do
+  its(:stdout) { should match /\/var\/nfs\s+192\.168\.6\.3/ }
+end
